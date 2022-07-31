48113a61f0242f6f5e1bf83a6d725c88a64c1442
pool <loop.edward@gmail.com>
2022-07-31 14:57:28 +0800

    Externalize path to points JSON file

--- src/main/kotlin/com/example/projecteuclid/repository/GeoPositionRepository.kt
+++ src/main/kotlin/com/example/projecteuclid/repository/GeoPositionRepository.kt
@@ -3,6 +3,7 @@ package com.example.projecteuclid.repository
 import com.example.projecteuclid.domain.GeoPosition
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.util.ResourceUtils
 import java.nio.file.Files
@@ -10,8 +11,11 @@ import java.nio.file.Files
 @Component
 class GeoPositionRepository {
 
+    @Value("\${repository.points.fileLocation}")
+    private lateinit var pointsFileLocation: String
+
     fun findAll(): List<GeoPosition> {
-        val pointsFile = ResourceUtils.getFile("classpath:points.json")
+        val pointsFile = ResourceUtils.getFile(pointsFileLocation)
         val contents = String(Files.readAllBytes(pointsFile.toPath()))
 
         val mapper = jacksonObjectMapper()
